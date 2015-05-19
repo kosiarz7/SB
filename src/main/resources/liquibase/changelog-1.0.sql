d2b400fe3b714963c381c7925b5029b1b01090a4
qbisiak <qbisiak@gmail.com>
2015-05-19 18:32:39 +0200

    Dodane dane do liquidbase. Poprawiony błąd przy mapowaniu encji hibernate.

--- src/main/resources/liquibase/changelog-1.0.sql
+++ src/main/resources/liquibase/changelog-1.0.sql
@@ -8,28 +8,44 @@ CREATE TABLE obywatelstwa (
 );
 
 CREATE TABLE klienci (
-	id_klient INT PRIMARY KEY,
-	imie VARCHAR2(64) NOT NULL,
-	nazwisko VARCHAR2(64) NOT NULL,
-  	login VARCHAR(100) NOT NULL,
-  	haslo VARCHAR(128) NOT NULL,
-  	nieudane_logowania INTEGER DEFAULT 0 NOT NULL,
-  	adres VARCHAR2(64) NOT NULL,
-  	kod_pocztowy VARCHAR2(6) NOT NULL,
-  	miasto VARCHAR2(32) NOT NULL,
-  	pesel VARCHAR2(11)  NOT NULL,
-  	nr_dowodu VARCHAR2(10) NOT NULL,
-  	id_obywatelstwa INT NOT NULL,
-  	email VARCHAR2(128),
-  	CONSTRAINT klienci_dl_imie CHECK (LENGTH(imie) > 2),
-  	CONSTRAINT klienci_dl_nazwisko CHECK (LENGTH(nazwisko) > 2), 
-  	CONSTRAINT klienci_dl_adres CHECK (LENGTH(adres) > 5), 
-  	CONSTRAINT klienci_dl_kod_pocztowy CHECK (REGEXP_LIKE(kod_pocztowy, '^[0-9]{2}[-]{1}[0-9]{3}$')), 
-  	CONSTRAINT klienci_dl_miasto CHECK (LENGTH(miasto) > 2), 
-  	CONSTRAINT klienci_pesel CHECK (REGEXP_LIKE(pesel, '^[0-9]{11}$')), 
-  	CONSTRAINT klienci_nr_dowodu CHECK (REGEXP_LIKE(nr_dowodu, '^[A-Z]{3}[-]{1}[0-9]{6}$')),
-  	CONSTRAINT klienci_fk_obywatelstwo FOREIGN KEY (id_obywatelstwa) REFERENCES obywatelstwa(id_obywatelstwa)
+id_klient INT PRIMARY KEY,
+  imie VARCHAR2(64) NOT NULL,
+  nazwisko VARCHAR2(64) NOT NULL,
+  login VARCHAR(100) NOT NULL,
+  haslo VARCHAR(128) NOT NULL,
+  nieudane_logowania INTEGER DEFAULT 0 NOT NULL,
+  adres VARCHAR2(64) NOT NULL,
+  kod_pocztowy VARCHAR2(6) NOT NULL,
+  miasto VARCHAR2(32) NOT NULL,
+  pesel VARCHAR2(11)  NOT NULL,
+  nr_dowodu VARCHAR2(10) NOT NULL,
+  id_obywatelstwa INT NOT NULL,
+  email VARCHAR2(128) NOT NULL,
+  CONSTRAINT klienci_dl_imie CHECK (LENGTH(imie) > 2),
+  CONSTRAINT klienci_dl_nazwisko CHECK (LENGTH(nazwisko) > 2), 
+  CONSTRAINT klienci_dl_adres CHECK (LENGTH(adres) > 5), 
+  CONSTRAINT klienci_dl_kod_pocztowy CHECK (REGEXP_LIKE(kod_pocztowy, '^[0-9]{2}[-]{1}[0-9]{3}$')), 
+  CONSTRAINT klienci_dl_miasto CHECK (LENGTH(miasto) > 2), 
+  CONSTRAINT klienci_pesel CHECK (REGEXP_LIKE(pesel, '^[0-9]{11}$')), 
+  CONSTRAINT klienci_nr_dowodu CHECK (REGEXP_LIKE(nr_dowodu, '^[A-Z]{3}[-]{1}[0-9]{6}$')),
+  CONSTRAINT klienci_fk_obywatelstwo FOREIGN KEY (id_obywatelstwa) REFERENCES obywatelstwa(id_obywatelstwa)
 );
+
+CREATE TABLE rachunki (
+  id_rachunek INT PRIMARY KEY,
+  numer  VARCHAR2(26) NOT NULL,
+  nazwa VARCHAR(255) NOT NULL,
+  saldo DECIMAL(*, 5) NOT NULL ,
+  enabled VARCHAR2(1) NOT NULL,
+  data_zalozenia DATE NOT NULL
+);
+
+CREATE TABLE klienci_rachunki (
+  id_klient INT NOT NULL,
+  id_rachunek INT  NOT NULL,
+  CONSTRAINT klienci_rachunki_fk_klient FOREIGN KEY (id_klient) REFERENCES klienci(id_klient),
+  CONSTRAINT klienci_rachunki_fk_rachunek FOREIGN KEY (id_rachunek) REFERENCES rachunki(id_rachunek)
+); 
 
 CREATE TABLE uprawnienia (
     id INTEGER NOT NULL,
@@ -38,29 +54,11 @@ CREATE TABLE uprawnienia (
 );
 
 CREATE TABLE uprawnienia_klientow (
-    id INTEGER NOT NULL,
     klient_id INTEGER NOT NULL,
     uprawnienie_id INTEGER NOT NULL,
-    PRIMARY KEY (ID),
     CONSTRAINT uprawnienia_kli_00_klient_fk FOREIGN KEY (klient_id) REFERENCES klienci(id_klient),
     CONSTRAINT uprawnienia_kli_00_upraw_fk FOREIGN KEY (uprawnienie_id) REFERENCES uprawnienia(id)
 );
-
-CREATE TABLE rachunki (
-  	id_rachunek VARCHAR2(26) PRIMARY KEY,
-  	nazwa VARCHAR(255) NOT NULL,
-  	numer INTEGER NOT NULL,
-  	saldo DECIMAL(*, 5) NOT NULL ,
-  	data_zalozenia DATE NOT NULL
-);
-
-CREATE TABLE klienci_rachunki (
-  	id_klient_rachunek INT PRIMARY KEY,
-  	id_klient INT NOT NULL,
-  	id_rachunek VARCHAR2(26)  NOT NULL,
-  	CONSTRAINT klienci_rachunki_fk_klient FOREIGN KEY (id_klient) REFERENCES klienci(id_klient),
-  	CONSTRAINT klienci_rachunki_fk_rachunek FOREIGN KEY (id_rachunek) REFERENCES rachunki(id_rachunek)
-); 
 
 INSERT INTO obywatelstwa(id_obywatelstwa, obywatelstwo) VALUES (1, 'polskie');
 COMMIT;
---
+++ src/main/resources/liquibase/changelog-2.0.sql
@@ -0,0 +1,39 @@
+--liquibase formatted sql
+
+--changeset samoSieZrobilo:2
+CREATE TABLE typUbezpieczenie(
+	id_typUbezpieczenie integer  NOT NULL,
+  	nazwa varchar2(50)  NOT NULL,
+  	typ varchar2(50)  NOT NULL,
+	opis_korzysci varchar2(255)  NOT NULL,
+  	opis_inne varchar2(255) NOT NULL,
+	CONSTRAINT typUbezpieczenie_pk PRIMARY KEY (id_typUbezpieczenie)
+);
+
+INSERT INTO typUbezpieczenie (id_typUbezpieczenie, nazwa, typ, opis_korzysci, opis_inne) VALUES (1, 'Ubezpieczenie komunikacyjne', 'komunikacyjne',
+'Przenosisz swoje zniżki za bezszkodową jazdę. Przenosisz zniżki z OC na AC. 5% zniżki przy zakupie ubezpieczenia przez internet. Płacisz niskie, miesięczne raty, o których pamięta za Ciebie bank.',
+'Możesz kupić polisę nawet z 3-miesięcznym wyprzedzeniem. Ubezpieczenie kupujesz przez internet lub telefon, bez wychodzenia z domu.');
+INSERT INTO typUbezpieczenie (id_typUbezpieczenie, nazwa, typ, opis_korzysci, opis_inne) VALUES (2, 'Ubezpieczenie turystyczne', 'zdrowotne',
+'Wybór jednego z trzech wariantów w formie indywidualnej, rodzinnej lub grupowej. Ochrona już po dwóch godzinach od złożenia wniosku. Numer polisy wysyłany SMS-em.',
+'Ubezpieczenie obejmuje m.in.: koszty leczenia, koszty transportu medycznego, NNW, OC. Terytorium: Europa lub cały świat.');
+INSERT INTO typUbezpieczenie (id_typUbezpieczenie, nazwa, typ, opis_korzysci, opis_inne) VALUES (3, 'Waleczni', 'zdrowotne',
+'Finansowe zabezpieczenie na wypadek nowotworu, w przypadku braku dochodów i dodatkowych kosztów m.in. leków, dotarcia do głównych ośrodków diagnostycznych, specjalistycznych klinik, rehabilitacji.',
+'Nie wymagamy badań lekarskich. Przystąpienie do umowy nie wymaga Twojego podpisu. Miesięczna kwota za ubezpieczenia będzie pobierana automatycznie z konta.');
+INSERT INTO typUbezpieczenie (id_typUbezpieczenie, nazwa, typ, opis_korzysci, opis_inne) VALUES (4, 'Ubezpieczenie nieruchomości', 'nieruchomości',
+'Wysoka suma ubezpieczenia na wyposażenie. Niskie miesięczne opłaty, o których pamiętamy za Ciebie.',
+'Możliwość zakupu polisy nawet z 90-o dniowym wyprzedzeniem. Wysłanie polisy na adres e-mail w ciągu doby od zakupu.');
+INSERT INTO typUbezpieczenie (id_typUbezpieczenie, nazwa, typ, opis_korzysci, opis_inne) VALUES (5, 'Mieszkanie 24h', 'nieruchomości',
+'Możliwość wyboru jednego z 3 wariantów ubezpieczenia. Niska cena - miesięczna opłata już od 19,90 PLN.',
+'Uzyskasz odszkodowanie, gdy Twoje mieszkanie zostanie zalane lub strawi je pożar. Odzyskasz środki, jeśli padniesz ofiarą kradzieży, rabunku lub dewastacji. Nie poniesiesz kosztów, jeżeli zalejesz sąsiada lub spowodujesz inną szkodę.');
+INSERT INTO typUbezpieczenie (id_typUbezpieczenie, nazwa, typ, opis_korzysci, opis_inne) VALUES (6, 'NNW Ochrona 24h', 'zdrowotne',
+'Podstawowa ochrona już od 3,99 PLN miesięcznie. Wysokie sumy ubezpieczenia, nawet 200 000 PLN.',
+'Wypłata świadczenia w przypadku pobytu w szpitalu lub śmierci w wyniku nieszczęśliwego wypadku (w tym w środku lokomocji).');
+INSERT INTO typUbezpieczenie (id_typUbezpieczenie, nazwa, typ, opis_korzysci, opis_inne) VALUES (7, 'Assistance Dom 24h', 'nieruchomości',
+'Pomoc w przypadku nagłej awarii, włamania, kradzieży, pożaru lub innych, nieprzewidzianych okoliczności w miejscu zamieszkania.',
+'Szybka pomoc w przypadku uszkodzenia mienia w miejscu zamieszkania wskutek zdarzenia losowego lub kradzieży z włamaniem.');
+INSERT INTO typUbezpieczenie (id_typUbezpieczenie, nazwa, typ, opis_korzysci, opis_inne) VALUES (8, 'Bezpieczna karta', 'karty',
+'Ochrona w zakresie nieuprawnionego użycia karty i utraty środków pieniężnych.',
+'Transakcje dokonane przed zastrzeżeniem karty oraz nieuprawnione transakcje przy użyciu numeru PIN. Obejmuje utratę ubezpieczonej gotówki do 2h od jej podjęcia.');
+INSERT INTO typUbezpieczenie (id_typUbezpieczenie, nazwa, typ, opis_korzysci, opis_inne) VALUES (9, 'Ubezpieczenie splaty kredytu', 'kredytu',
+'Zawarcie ubezpieczenia umożliwa negocjowanie oprocentowania kredytu gotówkowego i prowizji za udzielenie. Poczucie finansowego bezpieczeństwa - realna i bezzwrotna pomoc finansowa',
+'Obejmuje calkowitą trwala niezdolność do pracy w wyniku choroby lub nieszcześliwego wypadku, pobyt w szpitalu, poważne zachorowania i operacje chirurgiczne.');
