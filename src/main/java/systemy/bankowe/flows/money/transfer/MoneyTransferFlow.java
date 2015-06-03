5a07b996a6eb307f16f020fadfb701643a4b86c0
akopaczewski <kopaczewski.adam@gmail.com>
2015-06-03 12:13:48 +0200

    Wybór zdefiniowanego odbiorcy w trakcie przelewu. Przejście do okna przelewu z wybranego konta.

--- src/main/java/systemy/bankowe/flows/money/transfer/MoneyTransferFlow.java
+++ src/main/java/systemy/bankowe/flows/money/transfer/MoneyTransferFlow.java
@@ -5,19 +5,51 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.hibernate.SessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import systemy.bankowe.dao.transfer.WaitingTransferDao;
 import systemy.bankowe.dto.AccountDto;
+import systemy.bankowe.dto.DefinedRecipientDto;
 import systemy.bankowe.dto.UserDto;
 import systemy.bankowe.dto.transfer.OutcomingTransfer;
 import systemy.bankowe.dto.transfer.RealizedTransfer;
 import systemy.bankowe.dto.transfer.TransferType;
 import systemy.bankowe.dto.transfer.WaitingTransfer;
+import systemy.bankowe.security.SpringSecurityContextUtil;
 import systemy.bankowe.services.user.UserData;
+import systemy.bankowe.util.StringUtil;
 
 public class MoneyTransferFlow implements Serializable {
 
     private static final long serialVersionUID = 1L;
+    
+    private static final Logger LOGGER = LoggerFactory.getLogger(MoneyTransferFlow.class);
+    
+    private List<DefinedRecipientDto> recipients;
+    
+    public boolean isDefinedRecipeintsAvailable() {
+        if (null != recipients) {
+            return !recipients.isEmpty();
+        }
+        return false;
+    }
+    
+    public void init(SpringSecurityContextUtil loginUserService, String targetAccountNumber,
+            WaitingTransfer waitingTransfer) {
+        LOGGER.debug("init|targetAccountNumber: {}", targetAccountNumber);
+        recipients = loginUserService.getAllDefinedRecipients();
+        if (!StringUtil.isEmpty(targetAccountNumber)) {
+            waitingTransfer.setSenderAccountNumber(splitAccountNumber(targetAccountNumber));
+        }
+    }
+    
+    public void selectRecipientListener(DefinedRecipientDto selectedRecipient, WaitingTransfer waitingTransfer) {
+        waitingTransfer.setTargetName(selectedRecipient.getName());
+        waitingTransfer.setAddress(selectedRecipient.getStreet() + " " + selectedRecipient.getStreetNo() + " "
+                + selectedRecipient.getCity() + " " + selectedRecipient.getZipcode());
+        waitingTransfer.setTargetAccountNumber(splitAccountNumber(selectedRecipient.getAccountNumber()));
+    }
 
     public MoneyTransferResult sendMoneyTransfer(final WaitingTransfer aTransfer, SessionFactory factory, UserData user) {
         WaitingTransferDao waitingTransfer = new WaitingTransferDao();
@@ -72,5 +104,12 @@ public class MoneyTransferFlow implements Serializable {
         }
         return str.toString();
     }
-    
+
+    public List<DefinedRecipientDto> getRecipients() {
+        return recipients;
+    }
+
+    public void setRecipients(List<DefinedRecipientDto> recipients) {
+        this.recipients = recipients;
+    }
 }
